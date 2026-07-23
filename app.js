12924a6db04b0305918b557ed77ba3177f767cb9
Your Name <you@example.com>
2026-07-23 16:12:26 -0500

    metodo commonJS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
+
 app.get("/", (_, res) => {
-    res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
+    res.send("Hola, estamos aprendiendo Express con la ficha 3407184");
 });
-app.listen(port, () => { 
-    console.log( `Servidor en funcionamiento en el puerto:  `); 
-}); 
+
+app.listen(port, () => {
+    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+});
