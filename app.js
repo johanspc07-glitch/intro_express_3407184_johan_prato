7ec7979c16a05c79751ed43c378326f5b924a02f
jassjy <s4nsy150@gmail.com>
2026-07-30 13:37:22 -0500

    listado de productos

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import {configDotenv} from 'dotenv';
 configDotenv();
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get("/", (_, res) => {
     res.send("Hola, estamos aprendiendo Express con la ficha 3407184");
 });
+
+app.get("/productos", (req, res)=>{
+    res.send(`<h1> Listado de productos</h1>
+        <ol>
+        <li>Televisor</li>
+        <li>Celular</li>
+        <li>Impresora</li>
+        </ol>
+    `)
+})
 
 app.listen(port, () => {
     console.log(`Servidor en funcionamiento en el puerto: ${port}`);
