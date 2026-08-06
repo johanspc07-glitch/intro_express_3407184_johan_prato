1c93d13e948dd65f255a5d680014bb83993bf6d3
jassjy <s4nsy150@gmail.com>
2026-08-06 16:36:25 -0500

    rama commonjs

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get("/", (_, res) => {
-    res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
+require ("dotenv").config();
+const express = require('express');  
+const app = express();  
+const port =  process.env.PORT || 3000; 
+
+app.get("/", (_, res) => { 
+  res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
+});  
+
+app.get("/datos", (_, res) => {
+  res.json({
+    datos_personales: {
+      nombre: "Johan Stiven",
+      apellido: "Prato Cuesta",
+      listatelefono: [3102218250]
+    },
+    datos_programa: {
+      nombre_programa: "ADSO",
+      tipo_programa: "tecnologo",
+      ficha: "3407184"
+    }
+  });
 });
+
+
 app.listen(port, () => { 
-    console.log( `Servidor en funcionamiento en el puerto:  `); 
-}); 
+  console.log(`Servidor en funcionamiento en el puerto: ${port}`); 
+});
